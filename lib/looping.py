ae06b1a59de01d8e77d9f958f068348280406801
Error7799 <kingy7799sentamu@gmail.com>
2024-09-02 21:27:43 +0300

    i like loops

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,35 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    num = 10
+    while num > 1:
+        print(num)
+        num = num - 1
+    print("Happy New Year!")
+        
+        
+pass
 
 def square_integers(int_list):
     # code goes here!
+    return[num ** 2 for num in int_list]
     pass
 
 def fizzbuzz():
     # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
+
+fizzbuzz()
+
+        
+    
+
